54520397e667e7818cc14fbbfa2e243fd1cf2765
steyerzalan <steyerzalan@gmail.com>
2025-03-25 08:36:46 +0100

    táblalétrehozás teszt előtt

--- tablaletrehozasa.php
+++ tablaletrehozasa.php
@@ -15,6 +15,36 @@ if ($conn->connect_error)
 }
 else
 {
-    echo "Csatlakozás megtörtént";
+    echo "Csatlakozás megtörtént!";
 }
+
+//tábla létrehozása
+$sql= "DROP TABLE if EXISTS emberek";
+if ($conn->query($sql) === true)
+{
+    echo "Sikeres táblatörlés";
+}
+else
+{
+    echo "Nem sikerült a táblatörlés";
+}
+$sql="";
+$sql ="CREATE TABLE if NOT EXISTS emberek(
+    id INT unsigned AUTO_INCREMENT PRIMARY KEY,
+    vezeteknev VARCHAR(30) NOT NULL,
+    keresztnev VARCHAR(30) NOT NULL,
+    beosztas VARCHAR(50) NOT NULL,
+    regiszt_datum TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+if ($conn->query($sql) === true)
+{
+    echo "Sikeres létrehozás";
+}
+else
+{
+    echo "Nem sikerült a táblalétrehozás";
+}
+
+//lezárni az adatbázis kapcsolatot, erőforrást felszabadítunk
+$conn->close();
 
